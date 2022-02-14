60dc0f9c1f6ad3caf96df022ca20a207a14b178d
Michael Mogessie <michaelmogessie@gmail.com>
2022-02-13 22:59:51 -0500

    First commit

--- build_script.sh
+++ build_script.sh
@@ -3,4 +3,4 @@ docker rm pubsubdemo-docker || true
 docker rmi pubsubdemo-docker || true
 mvn clean package -DskipTests -Pprod
 docker build -t pubsubdemo-docker:latest .
-docker run -p 80:80 -d --name pubsubdemo-docker pubsubdemo-docker
+docker run -p 8091:8091 -d --name pubsubdemo-docker pubsubdemo-docker
